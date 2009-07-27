a875de121c6de986ab50ae7ddcade225020c22e9
eero <eero@eero-laptop>
2009-07-27 11:45:05 +0300

    Initial commit

--- src/main/scala/eero/chat/comet/UserActor.scala
+++ src/main/scala/eero/chat/comet/UserActor.scala
@@ -1,10 +1,19 @@
 package eero.chat.comet
 
+import eero.chat.model._
+
 import scala.actors._
 import scala.xml._
+import scala.collection.mutable.ListBuffer
+
 import net.liftweb.http._
+
 
 class UserActor extends CometActor
 {
-	def render = { new Text("Hello")}
+  
+  var messages = new ListBuffer[Message]
+
+  def render = { new Text("Hello")}
+  def renderMsgs = { new Text("Hulloe")}
 }
---
+++ src/test/scala/eero/chat/CometActorTest.scala
@@ -0,0 +1,41 @@
+package eero.chat
+
+import _root_.junit.framework._
+import Assert._
+import _root_.scala.xml._
+import _root_.net.liftweb.util._
+
+import java.lang.Thread
+
+import eero.chat.model._
+import eero.chat.comet._
+
+import scala.actors._
+import scala.actors.Actor._
+
+class CometActorTest extends TestCase
+{
+  var nick = "Eero"
+  var msgTxt = "Hello World!"
+  var user = User.create.nick(nick)
+  var hello = Message.create.sender(user).text(msgTxt)
+  var actor = new UserActor
+  
+  override def setUp =
+  {
+	  actor = new UserActor
+	  actor.start
+  }
+  
+  def parseNick(msg:NodeSeq) = msg.text.split(":").first
+  
+  def testSingleMessage = 
+  {
+      actor ! Send (hello)
+      Thread.sleep(100)
+      var msgs:NodeSeq = actor.renderMsgs
+      var gotNick = parseNick(msgs)
+      assertEquals("Expected nick not found", nick, gotNick)
+  }
+  
+}
